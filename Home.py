759054f1c3311d49db56d97e9e1f2aa805cf0475
SABDULLAH42 <142105865+SABDULLAH42@users.noreply.github.com>
2026-05-12 10:48:14 +0530

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Gurgaon Real Estate Analytics App",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Streamlit! 👋")
+
+st.sidebar.success("Select a demo above.")
+
+
+# streamlit run "C:/Users/USER/Desktop/UDEMY/Capstone1 Project ML/Home.py"
